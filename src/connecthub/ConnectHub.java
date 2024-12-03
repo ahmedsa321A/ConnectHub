483fa96e920716a6679019a0ae2642e395039cf5
Adham-zakaria <es-adham.zakaria2027@alexu.edu.eg>
2024-12-03 09:26:05 +0200

    Revert "Merge pull request #2 from ahmedsa321A/ContentCreation"
    This reverts commit 3706b542ba0bd4e90d49a80b529a9147b4716a6b, reversing
    changes made to 98b2dc84058e5a56041998e1b3808141908a65f6.

---
+++ src/connecthub/ConnectHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package connecthub;
+
+/**
+ *
+ * @author ahmed
+ */
+public class ConnectHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login log=new Login();
+    }
+    
+}
