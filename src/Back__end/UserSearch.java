bea1fb5d44974c3745714396cc684d4e594c42b3
ahmedsa321A <ahmedsa321A@gmail.com>
2024-12-05 04:21:35 +0200

    Update UserSearch.java

--- src/Back__end/UserSearch.java
+++ src/Back__end/UserSearch.java
@@ -1,16 +1,19 @@
 package Back__end;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class UserSearch {
-    private HashMap<String, User> userMap;
+    public static HashMap<String, User> userMap=new HashMap<>(); //hash map
         private static final String DATABASE_FILE = "user_db.json";
-    
+    public void setAllMap(ArrayList<User> userList) {
+        if (userList != null) {
+            for (User user : userList) {
+                userMap.put(user.getUserId(), user);
+            }
+        }
+    }
     public void setUserMap(HashMap<String, User> userMap) {
         this.userMap = userMap;
     }
@@ -19,22 +22,4 @@ public class UserSearch {
         return userMap.get(userId);
     }
     
-    public static User getUserByIdFromJson(String userId) {
-        try (FileReader reader = new FileReader(DATABASE_FILE)) {
-            // Parse the JSON file
-            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
-
-            // Check if the user with the given ID exists in the JSON file
-            if (jsonObject.has(userId)) {
-                Gson gson = new Gson();
-                return gson.fromJson(jsonObject.get(userId), User.class);
-            } else {
-                System.out.println("User not found with ID: " + userId);
-                return null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
