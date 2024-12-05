ba7d7b90ad1eb5efaa6b44f99b46100a9abd56e5
ahmedsa321A <ahmedsa321A@gmail.com>
2024-12-05 18:23:19 +0200

    Changed news feed window

--- src/Front__end/NewsFeedWindow.java
+++ src/Front__end/NewsFeedWindow.java
@@ -8,11 +8,12 @@ import Back__end.RelationshipStatus;
 import Back__end.User;
 import Back__end.UserRepository;
 import Back__end.UserSearch;
-import Back__end.userService;
 import com.google.gson.reflect.TypeToken;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -27,6 +28,13 @@ public class NewsFeedWindow extends javax.swing.JFrame {
 
     public NewsFeedWindow(User user) {
         initComponents();
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+             user.setStatus("offline");
+             UserRepository.saveData();
+            }
+        });
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (screenSize.width - this.getWidth()) / 2;
         int y = (screenSize.height - this.getHeight()) / 2;
@@ -262,35 +270,33 @@ StoryGui story = new StoryGui(user);
         this.dispose();    }//GEN-LAST:event_storyActionPerformed
 
     private void friendsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_friendsActionPerformed
-    // Initialize a loader for friends
+
     FriendLoader load = new FriendLoader();
 
-    // Specify the type for deserialization (List<User>)
+
         java.lang.reflect.Type typeOfT = new TypeToken<List<User>>() {}.getType();
 
-    // Load friends and relationship data
+
     load.loadFromFile(user, "user_db.json", typeOfT);
 
-    // Set up a UserSearch map
+
     UserSearch search = new UserSearch();
     search.setAllMap(UserRepository.userList);
 
-    // Iterate through the user map to find suggestions
+
     for (Map.Entry<String, User> entry : search.getMap().entrySet()) {
         User otherUser = entry.getValue();
 
-        // Skip the current user
+
         if (!user.getUserId().equals(otherUser.getUserId())) {
             RelationshipStatus status = RelationshipManager.getRelationshipStatus(user, otherUser);
 
-            // Add to suggestions if not friends
             if (status == RelationshipStatus.NOT_FRIENDS) {
                 user.addSuggestion(otherUser.getUserId());
             }
         }
     }
 
-    // Pass the updated user map to the FriendsCenter
     FriendsCenter friends = new FriendsCenter(user, search.getMap());
     setVisible(false);
     }//GEN-LAST:event_friendsActionPerformed
