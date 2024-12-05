8b0722be88d3057f04164b40348564f355d0ae9b
ahmedsa321A <ahmedsa321A@gmail.com>
2024-12-05 05:38:03 +0200

    Revert "Update UserSearch.java"
    This reverts commit b562a35b5cc3b598d931fcb403f27651c0251a78.

--- src/Back__end/UserSearch.java
+++ src/Back__end/UserSearch.java
@@ -1,15 +1,10 @@
 package Back__end;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 
 public class UserSearch {
     private HashMap<String, User> userMap;
-        private static final String DATABASE_FILE = "user_db.json";
+
     
     public void setUserMap(HashMap<String, User> userMap) {
         this.userMap = userMap;
@@ -17,24 +12,5 @@ public class UserSearch {
 
     public User getUserById(String userId) {
         return userMap.get(userId);
-    }
-    
-    public static User getUserByIdFromJson(String userId) {
-        try (FileReader reader = new FileReader(DATABASE_FILE)) {
-            // Parse the JSON file
-            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
-
-            // Check if the user with the given ID exists in the JSON file
-            if (jsonObject.has(userId)) {
-                Gson gson = new Gson();
-                return gson.fromJson(jsonObject.get(userId), User.class);
-            } else {
-                System.out.println("User not found with ID: " + userId);
-                return null;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
     }
 }
