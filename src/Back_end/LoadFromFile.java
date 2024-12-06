1051520df0724957ba3796ce25de68d9a99ed33f
ahmedsa321A <ahmedsa321A@gmail.com>
2024-12-06 13:27:06 +0200

    Cleanup
    Removed all files from main branch to prepare for final merge

--- src/Back_end/LoadFromFile.java
+++
@@ -1,9 +0,0 @@
-
-package Back_end;
-
-import java.lang.reflect.Type;
-
-//Load from file interface
-public interface LoadFromFile {
-    public void loadFromFile(FriendUser currentUser,String filePath, Type typeOfT);
-}
--- src/Back_end/SaveToFile.java
+++
@@ -1,7 +0,0 @@
-
-package Back_end;
-
-//save to file interface
-public interface SaveToFile {
-    void saveToFile(FriendUser data, String filePath);
-}
